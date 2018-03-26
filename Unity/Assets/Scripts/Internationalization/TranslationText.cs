2eb1337ca6f90b58f497b1dbac0c78d20faad3e7
Jip Sterk <JipSterk@users.noreply.github.com>
2018-03-26 13:58:26 +0200

    Added Internationalization support

---
+++ Unity/Assets/Scripts/Internationalization/TranslationText.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace RaceGame.Internationalization
+{
+    public class TranslationText : MonoBehaviour
+    {
+        public string Key;
+
+        private TextMeshProUGUI _textMeshProUgui;
+
+        private void OnEnable() => I18N.OnLocaleChanged.AddListener(SetText);
+
+        private void Start()
+        {
+            if (!_textMeshProUgui)
+                _textMeshProUgui = GetComponent<TextMeshProUGUI>();
+
+            SetText();
+        }
+
+        private void SetText() => _textMeshProUgui.__(Key);
+
+        private void OnDisable() => I18N.OnLocaleChanged.RemoveListener(SetText);
+    }
+}
