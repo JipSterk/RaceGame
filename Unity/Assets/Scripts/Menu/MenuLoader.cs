4697da92433f1c2bcde1e5507a1f166609503613
Jip Sterk <JipSterk@users.noreply.github.com>
2018-03-26 13:04:36 +0200

    Added basic menu panels

---
+++ Unity/Assets/Scripts/Menu/MenuLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RaceGame.Menu
+{
+    public class MenuLoader : MonoBehaviour
+    {
+        [SerializeField] private MenuPanel _menuPanel;
+
+        private void Start() => _menuPanel.MoveInToViewPort();
+    }
+}
---
+++ Unity/Assets/Scripts/Menu/MenuPanel.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace RaceGame.Menu
+{
+    public class MenuPanel : MonoBehaviour
+    {
+        [SerializeField] private GameObject _firstSelected;
+        [SerializeField] private float _duration;
+
+        private Vector3 _resetPosition;
+        
+        private void Start()
+        {
+            _resetPosition = transform.position;
+        }
+
+        public void MoveInToViewPort()
+        {
+            transform.DOMove(new Vector3(0, 0, transform.position.z), _duration);
+            EventSystem.current.SetSelectedGameObject(_firstSelected);
+        }
+
+        public void MoveOutOfViewPort() => transform.DOMove(_resetPosition, _duration);
+    }
+}
